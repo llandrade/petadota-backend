d7a3ed7de4e8238dab36d34f652e2c05dddf8910
leoandrade23 <leonardo.lopes.andrade@gmail.com>
2025-06-03 19:23:58 -0300

    Adicionando documentação nos métodos

--- src/main/java/com/example/petadota/controller/AnimalController.java
+++ src/main/java/com/example/petadota/controller/AnimalController.java
@@ -17,21 +17,45 @@ public class AnimalController {
         this.repository = repository;
     }
 
+    /**
+     * Metodo REST GET para listar animais
+     *
+     * @return Lista com animais
+     */
     @GetMapping
     public List<Animal> listarTodos() {
         return repository.findAll();
     }
 
+    /**
+     * Metodo REST POST para salvar um animal
+     *
+     * @param animal Animal para persistir
+     * @return Animal persistido
+     */
     @PostMapping
     public Animal salvar(@RequestBody Animal animal) {
         return repository.save(animal);
     }
 
+    /**
+     * Metodo REST GET para buscar um animal por ID
+     *
+     * @param id Id do Animal
+     * @return Animal encontrado
+     */
     @GetMapping("/{id}")
     public Animal buscarPorId(@PathVariable Long id) {
         return repository.findById(id).orElse(null);
     }
 
+    /**
+     * Metodo REST PUT para atualizar um animal
+     *
+     * @param id Id do animal para atualizar
+     * @param atualizarAnimal Animal Atualizado
+     * @return Animal atualizado
+     */
     @PutMapping("/{id}")
     public Animal atualizar(@PathVariable Long id, @RequestBody Animal atualizarAnimal) {
         return repository.findById(id)
@@ -46,6 +70,10 @@ public class AnimalController {
                 }).orElse(null);
     }
 
+    /**
+     * Metodo REST DELETE para deletar um animal
+     * @param id Id do animal para deletar
+     */
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
         repository.deleteById(id);
